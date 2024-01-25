cafb82bb42c94c1ed621767107b7f5df971f5752
Gustavo Vilas Boas <gutog2105@gmail.com>
2024-01-24 23:04:46 -0300

    chore: teste

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,27 +6,31 @@ import { env } from "../env";
 import { JWTZod } from "../types/jwt-types";
 
 export class auth {
-	private(req: Request, res: Response, next: NextFunction) {
-		let seccess = false;
-		const { authorization } = req.headers;
+  private(req: Request, res: Response, next: NextFunction) {
+    let seccess = false;
+    const { authorization } = req.headers;
 
-		if (authorization) {
-			const [authType, token] = authorization.split(" ");
-			if (authType === "Bearer") {
-				try {
-					const decoded = JWTZod.safeParse(jwt.verify(token, env.JWT_SECRET_KEY));
+    if (authorization) {
+      const [authType, token] = authorization.split(" ");
+      if (authType === "Bearer") {
+        try {
+          const decoded = JWTZod.safeParse(
+            jwt.verify(token, env.JWT_SECRET_KEY),
+          );
 
-					if (decoded.success && decoded.data.role === "client") {
-						seccess = true;
-					}
-				} catch (e) {}
-			}
-		}
+          if (decoded.success && decoded.data.role === "client") {
+            seccess = true;
+          }
+        } catch (e) {
+          console.log("error");
+        }
+      }
+    }
 
-		if (!seccess) {
-			throw new AppError("403 Forbidem", 403);
-		}
+    if (!seccess) {
+      throw new AppError("403 Forbidem", 403);
+    }
 
-		next();
-	}
+    next();
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express, { response } from "express";
 import { routes } from "./routes";
 import { AppError } from "./errors/AppErro";
 
+const teeste = "ola";
+
 const app = express();
 
 app.use(express.json());
@@ -10,17 +12,17 @@ app.use(express.json());
 app.use(routes);
 
 app.use((err: Error) => {
-	if (err instanceof AppError) {
-		return response.status(err.statusCode).json({
-			status: err.statusCode,
-			menssage: err.menssage,
-		});
-	}
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: err.statusCode,
+      menssage: err.menssage,
+    });
+  }
 
-	return response.status(500).json({
-		status: "Error 500",
-		menssage: `Internal server error - ${err.message}`,
-	});
+  return response.status(500).json({
+    status: "Error 500",
+    menssage: `Internal server error - ${err.message}`,
+  });
 });
 
 app.listen(8080, () => console.log("Server is running in port 8080 🔥🚀"));
