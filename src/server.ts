3fa0da9f7c92343817aa371d71c3bce2b4747bbd
Gustavo Vilas Boas <gutog2105@gmail.com>
2024-01-24 23:28:54 -0300

    chore: create scripts start and build

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,6 @@ import express, { response } from "express";
 import { routes } from "./routes";
 import { AppError } from "./errors/AppErro";
 
-const teeste = "ola!!";
-
 const app = express();
 
 app.use(express.json());
