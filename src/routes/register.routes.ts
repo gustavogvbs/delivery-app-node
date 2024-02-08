3d65813260f67b758140d23cc87ca1d64230cf85
JOAO G <joao-g-l-m@hotmail.com>
2024-02-08 11:00:47 -0300

    feat: create Client

--- src/routes/register.routes.ts
+++ src/routes/register.routes.ts
@@ -1,28 +1,32 @@
 import { Router, Request, Response } from "express";
 
-import { createProfileController } from "@modules/profile/useCases/createProfile";
 import { createTenantController } from "@modules/tenants/useCases/createTenants";
 import { createAdminController } from "@modules/user/useCases/createAdmin";
+import { createClientController } from "@modules/user/useCases/createClient";
 import { createDevController } from "@modules/user/useCases/createDev";
 
 import { auth } from "@middleware/auth";
 
 const registerRouter = Router();
 
-registerRouter.post("/user", (req: Request, res: Response) => {
-  return createProfileController.handle(req, res);
+registerRouter.post("/user", async (req: Request, res: Response) => {
+  await createClientController.handle(req, res);
 });
 
-registerRouter.post("/admin", (req: Request, res: Response) => {
-  createAdminController.handle(req, res);
+registerRouter.post("/admin", async (req: Request, res: Response) => {
+  await createAdminController.handle(req, res);
 });
 
-registerRouter.post("/tenant", auth.admin, (req: Request, res: Response) => {
-  createTenantController.handle(req, res);
-});
+registerRouter.post(
+  "/tenant",
+  auth.admin,
+  async (req: Request, res: Response) => {
+    await createTenantController.handle(req, res);
+  },
+);
 
-registerRouter.post("/dev", auth.admin, (req: Request, res: Response) => {
-  createDevController.handle(req, res);
+registerRouter.post("/dev", auth.admin, async (req: Request, res: Response) => {
+  await createDevController.handle(req, res);
 });
 
 export { registerRouter };
