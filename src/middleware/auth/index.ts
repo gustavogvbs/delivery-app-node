5b149d20c2705d57a550c1193a879ae070de1174
Gustavo Vilas Boas <gutog2105@gmail.com>
2024-02-22 16:35:43 -0300

    refactor: authenticated in middleware changes

--- src/middleware/auth/index.ts
+++ src/middleware/auth/index.ts
@@ -1,4 +1,3 @@
-import { USERS_ROLES } from "@src/enums/RoleEnum";
 import { Response, Request, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
@@ -9,87 +8,23 @@ import { AppError } from "@errors/AppErro";
 import { JWTZod } from "@type/jwtType";
 
 class authAccess {
-  client(req: Request, res: Response, next: NextFunction): void {
-    let seccess = false;
-    const { authorization } = req.headers;
+  admin(req: Request, __: Response, next: NextFunction): void {
+    const { token } = req.cookies;
+    const { role } = req.params;
 
-    if (authorization) {
-      const [authType, token] = authorization.split(" ");
-      if (authType === "Bearer") {
-        try {
-          const decoded = JWTZod.safeParse(
-            jwt.verify(token, env.JWT_SECRET_KEY),
-          );
+    if (token && token !== "") {
+      try {
+        const decoded = JWTZod.safeParse(jwt.verify(token, env.JWT_SECRET_KEY));
 
-          if (decoded.success && decoded.data.role === USERS_ROLES.CLIENT) {
-            seccess = decoded.success;
-          }
-        } catch (e) {
-          console.error("error");
+        if (decoded.success && decoded.data.role === role) {
+          next();
+        } else {
+          throw new AppError("403 Forbidem", 403);
         }
+      } catch (e) {
+        console.log(e);
       }
     }
-
-    if (!seccess) {
-      throw new AppError("403 Forbidem", 403);
-    }
-
-    next();
-  }
-  admin(req: Request, res: Response, next: NextFunction): void {
-    let seccess = false;
-    const { authorization } = req.headers;
-
-    if (authorization) {
-      const [authType, token] = authorization.split(" ");
-      if (authType === "Bearer") {
-        try {
-          const decoded = JWTZod.safeParse(
-            jwt.verify(token, env.JWT_SECRET_KEY),
-          );
-
-          if (decoded.success && decoded.data.role === USERS_ROLES.ADMIN) {
-            seccess = decoded.success;
-          }
-        } catch (e) {
-          console.error("error");
-        }
-      }
-    }
-
-    if (!seccess) {
-      throw new AppError("403 Forbidem", 403);
-    }
-
-    next();
-  }
-
-  tenant(req: Request, res: Response, next: NextFunction): void {
-    let seccess = false;
-    const { authorization } = req.headers;
-
-    if (authorization) {
-      const [authType, token] = authorization.split(" ");
-      if (authType === "Bearer") {
-        try {
-          const decoded = JWTZod.safeParse(
-            jwt.verify(token, env.JWT_SECRET_KEY),
-          );
-
-          if (decoded.success && decoded.data.role === USERS_ROLES.TENANT) {
-            seccess = decoded.success;
-          }
-        } catch (e) {
-          console.error("error");
-        }
-      }
-    }
-
-    if (!seccess) {
-      throw new AppError("403 Forbidem", 403);
-    }
-
-    next();
   }
 }
 
