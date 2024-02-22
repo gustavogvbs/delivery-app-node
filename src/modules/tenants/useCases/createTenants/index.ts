74bd072695403b12f7f1c5ff52b55d27b646832d
Gustavo Vilas Boas <gutog2105@gmail.com>
2024-02-22 17:52:23 -0300

    feat: config controllers

--- src/modules/tenants/useCases/createTenants/index.ts
+++ src/modules/tenants/useCases/createTenants/index.ts
@@ -1,6 +1,7 @@
 import { PrismaTenantRepository } from "@repositories/implementations/PrismaTenantRepository";
 import { PrismaUserRepository } from "@repositories/implementations/PrismaUserRepository";
 
+import { Authenticate } from "@utils/Authenticate";
 import { JwtApi } from "@utils/JwtApi";
 
 import { CreateTenantController } from "./CreateTenantsControllers";
@@ -9,12 +10,16 @@ import { CreateTenantUseCase } from "./CreateTenantsUseCases";
 const jwpApi = new JwtApi();
 const userRepository = new PrismaUserRepository();
 const tenantRepository = new PrismaTenantRepository();
+const authenticate = new Authenticate();
 
 const createTenantUseCase = new CreateTenantUseCase(
   userRepository,
   tenantRepository,
   jwpApi,
 );
-const createTenantController = new CreateTenantController(createTenantUseCase);
+const createTenantController = new CreateTenantController(
+  createTenantUseCase,
+  authenticate,
+);
 
 export { createTenantController, createTenantUseCase };
--- src/modules/user/useCases/createAdmin/index.ts
+++ src/modules/user/useCases/createAdmin/index.ts
@@ -1,5 +1,6 @@
 import { PrismaUserRepository } from "@repositories/implementations/PrismaUserRepository";
 
+import { Authenticate } from "@utils/Authenticate";
 import { JwtApi } from "@utils/JwtApi";
 
 import { CreateAdminController } from "./CreateAdminController";
@@ -7,8 +8,12 @@ import { CreateAdminUseCase } from "./CreateAdminUseCase";
 
 const userRepository = new PrismaUserRepository();
 const jwtApi = new JwtApi();
+const authenticate = new Authenticate();
 
 const createAdminUseCase = new CreateAdminUseCase(userRepository, jwtApi);
-const createAdminController = new CreateAdminController(createAdminUseCase);
+const createAdminController = new CreateAdminController(
+  createAdminUseCase,
+  authenticate,
+);
 
 export { createAdminController, createAdminUseCase };
