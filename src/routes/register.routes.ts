e3361a821572faf2acd3bb7b86037f02121f0542
Gustavo Vilas Boas <gutog2105@gmail.com>
2024-04-02 19:30:19 -0300

    fix

--- src/routes/register.routes.ts
+++ src/routes/register.routes.ts
@@ -1,12 +1,9 @@
-import { USERS_ROLES as role } from "@src/enums/RoleEnum";
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response } from "express";
 
 import { createTenantController } from "@modules/tenants/useCases/createTenants";
 import { createAdminController } from "@modules/user/useCases/createAdmin";
 import { createClientController } from "@modules/user/useCases/createClient";
 import { createDevController } from "@modules/user/useCases/createDev";
-
-import { auth } from "@middleware/auth";
 
 const registerRouter = Router();
 
@@ -18,24 +15,12 @@ registerRouter.post("/admin", async (req: Request, res: Response) => {
   await createAdminController.handle(req, res);
 });
 
-registerRouter.post(
-  "/tenant",
-  async (req: Request, __: Response, next: NextFunction) => {
-    auth.execute(req, next, role.ADMIN);
-  },
-  async (req: Request, res: Response) => {
-    await createTenantController.handle(req, res);
-  },
-);
+registerRouter.post("/tenant", async (req: Request, res: Response) => {
+  await createTenantController.handle(req, res);
+});
 
-registerRouter.post(
-  "/dev",
-  async (req: Request, __: Response, next: NextFunction) => {
-    auth.execute(req, next, role.ADMIN);
-  },
-  async (req: Request, res: Response) => {
-    await createDevController.handle(req, res);
-  },
-);
+registerRouter.post("/dev", async (req: Request, res: Response) => {
+  await createDevController.handle(req, res);
+});
 
 export { registerRouter };
